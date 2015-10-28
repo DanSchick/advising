8c8f33a4e06c7a9dc6a51f771d9ba03f1a0481e9
Danny Schick <dschick@uvm.edu>
2015-10-28 18:07:34 -0400

    Put the query in a function to pull out the exact course for the plan and semester

--- plan.php
+++ plan.php
@@ -1,12 +1,25 @@
-<?php
-include "top.php"; ?>
 
 <h1>All courses for my sample plan</h1>
 <?php
-$query = 'SELECT tblCourses.fldCourseName FROM tblCourses INNER JOIN tblSemesterPlan ON tblSemesterPlan.fnkCourseId=tblCourses.pmkCourseNumber INNER JOIN tbl4YP ON tblSemesterPlan.fnkPlanId=tbl4YP.pmkPlanId INNER JOIN tblStudent ON tblStudent.pmkNetID = tbl4YP.fnkNetId
- WHERE tblStudent.pmkNetID = ?';
-    $data = array("dschick");
-    $info2 = $thisDatabaseReader->select($query, $data, 1, 0, 0, 0, false, false);
+
+// this function handles getting courses by year, semester, and plan.
+// After we use this, we simply have to print out the data
+function getCoursesBySem($plan, $semester, $year){
+        include "top.php";
+        $query = 'SELECT tblCourses.fldCourseName FROM tblCourses INNER JOIN tblSemesterPlan ON tblSemesterPlan.fnkCourseId=tblCourses.pmkCourseNumber INNER JOIN tbl4YP ON tblSemesterPlan.fnkPlanId=tbl4YP.pmkPlanId INNER JOIN tblStudent ON tblStudent.pmkNetID = tbl4YP.fnkNetId
+        WHERE tbl4YP.pmkPlanId = ? AND tblSemesterPlan.fnkTerm = ? AND tblSemesterPlan.fldYear = ?';
+        $data = array($plan, $semester, $year);
+        $info2 = $thisDatabaseReader->select($query, $data, 1, 2, 0, 0, false, false);
+        return $info2;
+    }
+
+// $query = 'SELECT tblCourses.fldCourseName FROM tblCourses INNER JOIN tblSemesterPlan ON tblSemesterPlan.fnkCourseId=tblCourses.pmkCourseNumber INNER JOIN tbl4YP ON tblSemesterPlan.fnkPlanId=tbl4YP.pmkPlanId INNER JOIN tblStudent ON tblStudent.pmkNetID = tbl4YP.fnkNetId
+//  WHERE tblStudent.pmkNetID = ?';
+//     $data = array("dschick");
+//     $info2 = $thisDatabaseReader->select($query, $data, 1, 0, 0, 0, false, false);
+
+    $info2 = getCoursesBySem(1, 'Fall', '2015');
+    print_r($info2);
 
 
     $highlight = 1; // used to highlight alternate rows
