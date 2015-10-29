986df37be59c3122d6ef0ad0dc62c6ee4e4b347e
Danny Schick <dschick@uvm.edu>
2015-10-29 15:27:40 -0400

    plan will now automatically get semesters and years and print them out

--- plan.php
+++ plan.php
@@ -2,7 +2,7 @@
 <?php
     include "top.php";
 
-    /* 
+    /*
      * this function handles getting courses by year, semester, and plan.
      * After we use this, we simply have to print out the data
      */
@@ -12,9 +12,7 @@
         $dbUserName = get_current_user() . '_reader';
         $whichPass = "r"; //flag for which one to use.
         $dbName = DATABASE_NAME;
-
         $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
-
         $dbUserName = get_current_user() . '_writer';
         $whichPass = "w";
         $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
@@ -49,18 +47,46 @@
             }
             print '</tr>';
         }
-    print '</table>';
+        print '</table>';
     }
 
-    $fall2015 = getCoursesBySem(1, 'Fall', '2015');
-    $spring2016 = getCoursesBySem(1, 'Spring', '2016');
-    print '<h1>All courses for my sample plan</h1>';
-    print '<h2>Fall 2015</h2>';
-    print '<aisde>' . printQuery($fall2015) . '</aside>';
-    print '<br><h2>Spring 2016</h2>';
-    print '<aisde>' . printQuery($spring2016) . '</aside';
+    /*
+     * This function gets every semester and catalog year that a certain plan has.
+     * From this function we can determine how much to loop through
+     */
+    function getTerms($plan){
+        // first, we connect this function to the database
+        require_once('../bin/Database.php');
+        $dbUserName = get_current_user() . '_reader';
+        $whichPass = "r"; //flag for which one to use.
+        $dbName = DATABASE_NAME;
+        $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
+        $dbUserName = get_current_user() . '_writer';
+        $whichPass = "w";
+        $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
+
+        // now we execute query
+        $query = 'SELECT tblSemesterPlan.fnkTerm, tblSemesterPlan.fldYear FROM tblSemesterPlan INNER JOIN tblCourses ON tblSemesterPlan.fnkCourseId=tblCourses.pmkCourseNumber INNER JOIN tbl4YP ON tblSemesterPlan.fnkPlanId=tbl4YP.pmkPlanId INNER JOIN tblStudent ON tblStudent.pmkNetID = tbl4YP.fnkNetId
+        WHERE tbl4YP.pmkPlanId = ?';
+        $data = array($plan);
+        $info2 = $thisDatabaseReader->select($query, $data, 1, 0, 0, 0, false, false);
+        return $info2;
+    }
+
+    $planID = 1;
+    foreach (array_chunk(getTerms($planID), 2) as $chunk){
+        foreach($chunk as $pair){
+            $sem = $pair[0];
+            $year = $pair[1];
+
+        }
+
+        $q = getCoursesBySem($planID, $sem, $year);
+        print '<br><h2>' . $sem . ' ' . $year . '</h2>';
+        print '<aside>' . printQuery($q) . '</aside>';
 
 
 
-include "footer.php";
+    }
+
 ?>
