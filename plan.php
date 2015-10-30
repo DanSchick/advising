3ca766d1ac8d4d982c333fd5f03b5dbcc0d68c43
Danny Schick <dschick@uvm.edu>
2015-10-30 11:14:43 -0400

    Sorted term array so the plan displays in order

--- plan.php
+++ plan.php
@@ -29,7 +29,8 @@
 
     /*
      * This function prints the query that we received.
-     * We seperate getCourses and printQuery because there might be a case where we need to get the information but not print it
+     * We seperate getCourses and printQuery because there might be a case where we need to get the
+     * information but not print it
      */
     function printQuery($query){
         $highlight = 1; // used to highlight alternate rows
@@ -75,13 +76,54 @@
         return $info2;
     }
 
+        /**
+     * @param array $array
+     * @param string $value
+     * @param bool $asc - ASC (true) or DESC (false) sorting
+     * @param bool $preserveKeys
+     * @return array
+     * */
+    function sortBySubValue($array, $value, $asc = true, $preserveKeys = false) {
+        if ($preserveKeys) {
+            $c = array();
+            if (is_object(reset($array))) {
+                foreach ($array as $k => $v) {
+                    $b[$k] = strtolower($v->$value);
+                }
+            } else {
+                foreach ($array as $k => $v) {
+                    $b[$k] = strtolower($v[$value]);
+                }
+            }
+            $asc ? asort($b) : arsort($b);
+            foreach ($b as $k => $v) {
+                $c[$k] = $array[$k];
+            }
+            $array = $c;
+        } else {
+            if (is_object(reset($array))) {
+                usort($array, function ($a, $b) use ($value, $asc) {
+                    return $a->{$value} == $b->{$value} ? 0 : ($a->{$value} - $b->{$value}) * ($asc ? 1 : -1);
+                });
+            } else {
+                usort($array, function ($a, $b) use ($value, $asc) {
+                    return $a[$value] == $b[$value] ? 0 : ($a[$value] - $b[$value]) * ($asc ? 1 : -1);
+                });
+            }
+        }
+
+        return $array;
+    }
+
+
     /*
      * Now, we print out the courses for each term in a certain plan.
      * We use _get to be able to view different plans
      */
     $planID = (int) $_GET['plan'];
     print '<br><h1>Plan ID: ' . $planID . '</h1>';
-    foreach (getTerms($planID) as $chunk){
+    $planTerms = sortBySubValue(getTerms($planID), 'fldYear', true, false);
+    foreach ($planTerms as $chunk){
         $year = $chunk[0];
         $sem = $chunk[1];
         $q = getCoursesBySem($planID, $sem, $year);
